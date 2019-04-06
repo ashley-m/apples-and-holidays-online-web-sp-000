e59a94c62d1b56390a333f8c56d39f1ee7488296
Ashley Miller <asm2fe@virginia.edu>
2019-04-06 18:56:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
         list_string += holiday_hash[x].to_s
         list_string += ": \n"
         list_string += holiday_hash[x][y].to_s << ": \n"
-        holiday_hash[x][y].each_entry {|k| list_string << holiday_hash[x][y][k].to_s}
+        holiday_hash[x][y].each_entry {|k| list_string << holiday_hash[x][y][k]}
         list_string.chomp(-2)
       end
     end
